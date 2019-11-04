cd051a06a1321f622103a78e489a2649953c78d7
Manoel de Oliveira Prado Neto <manoelprado.aecjr@gmail.com>
2019-11-04 12:36:35 -0300

    authenticationLoading

--- src/store/modules/auth/reducer.js
+++ src/store/modules/auth/reducer.js
@@ -7,15 +7,25 @@ const INITIAL_STATE = {
 };
 
 export default function auth(state = INITIAL_STATE, action) {
-  switch (action.type) {
-    case '@auth/SIGN_IN_SUCCESS':
-      return produce(state, draft => {
+  return produce(state, draft => {
+    switch (action.type) {
+      case '@auth/SIGN_IN_REQUEST': {
+        draft.loading = true;
+        break;
+      }
+      case '@auth/SIGN_IN_SUCCESS': {
         draft.token = action.payload.token;
         draft.signed = true;
-        /* neste reducer de auth estamos armazenando apenas o token , a informacao
+        draft.loading = false;
+        break;
+      }
+      /* neste reducer de auth estamos armazenando apenas o token , a informacao
         se o usuario esta logado ou nao, e a informacao de loading ou nao */
-      });
-    default:
-      return state;
-  }
+      case '@auth/SIGN_FAILURE': {
+        draft.loading = false;
+        break;
+      }
+      default:
+    }
+  });
 }
--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -3,30 +3,34 @@ import { takeLatest, call, put, all } from 'redux-saga/effects';
 import history from '~/services/history';
 import api from '~/services/api';
 
-import { signInSuccess } from './actions';
+import { signInSuccess, signFailure } from './actions';
 
 export function* signIn({ payload }) {
-  const { email, password } = payload;
+  try {
+    const { email, password } = payload;
 
-  /* estamos chamando um método q retorna uma promisse (q é assincrona) por
-  isso usamos o call() por volta: primeiro parametro é o método e os demais
-  parametros são os param que o método em questao precisa receber:
-  url e os dados que queremos enviar */
-  const response = yield call(api.post, 'sessions', {
-    email,
-    password,
-  });
+    /* estamos chamando um método q retorna uma promisse (q é assincrona) por
+    isso usamos o call() por volta: primeiro parametro é o método e os demais
+    parametros são os param que o método em questao precisa receber:
+    url e os dados que queremos enviar */
+    const response = yield call(api.post, 'sessions', {
+      email,
+      password,
+    });
 
-  const { token, user } = response.data;
+    const { token, user } = response.data;
 
-  if (!user.provider) {
-    console.tron.error('Usuário não é um prestador!');
-    return;
+    if (!user.provider) {
+      console.tron.error('Usuário não é um prestador!');
+      return;
+    }
+
+    yield put(signInSuccess(token, user));
+
+    history.push('/dashboard');
+  } catch (err) {
+    yield put(signFailure());
   }
-
-  yield put(signInSuccess(token, user));
-
-  history.push('/dashboard');
 }
 
 export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
