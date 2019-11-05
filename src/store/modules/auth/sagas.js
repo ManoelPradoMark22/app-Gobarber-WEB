7dd29cea1e2384c13ce10442b2de3a2a46b2091a
Manoel de Oliveira Prado Neto <manoelprado.aecjr@gmail.com>
2019-11-05 09:42:19 -0300

    applicationRegistration

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -13,7 +13,8 @@ export function* signIn({ payload }) {
     /* estamos chamando um método q retorna uma promisse (q é assincrona) por
     isso usamos o call() por volta: primeiro parametro é o método e os demais
     parametros são os param que o método em questao precisa receber:
-    url e os dados que queremos enviar */
+    url e os dados que queremos enviar.
+    a rota sessions é a rota da API para login */
     const response = yield call(api.post, 'sessions', {
       email,
       password,
@@ -37,4 +38,27 @@ export function* signIn({ payload }) {
   }
 }
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+
+    /* a rota users é a rota da API para criar um usuario */
+    yield call(api.post, 'users', {
+      name,
+      email,
+      password,
+      provider: true,
+    });
+
+    history.push('/');
+  } catch (err) {
+    toast.error('Falha no cadastro, verifique seus dados!');
+
+    yield put(signFailure());
+  }
+}
+
+export default all([
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+]);
