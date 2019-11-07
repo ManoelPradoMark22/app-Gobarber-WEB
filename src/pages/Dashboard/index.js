f9d17f1da4b62bc548ab8cff08eb6ff3ce7b8599
Manoel de Oliveira Prado Neto <manoelprado.aecjr@gmail.com>
2019-11-07 12:27:40 -0300

    sailingBetweenDays

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,18 +1,43 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
+import { format, subDays, addDays } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 import api from '~/services/api';
 
 import { Container, Time } from './styles';
 
 export default function Dashboard() {
+  /* valor padrão da data vai ser o new Date() (data atual) */
+  const [date, setDate] = useState(new Date());
+
+  const dateFormatted = useMemo(
+    /* lembre de usar aspas duplas, pq aspas simples é o q vc não quer formatar */
+    () => format(date, "d 'de' MMMM", { locale: pt }),
+    [date]
+    /* esse segundo parametro são as dependencias, ou seja, qnd eu quero q
+    esse dateFormatted seja recalculado? quando o date alterar! */
+  );
+
+  function handlePrevDay() {
+    /* setando um novo valor para o nosso estado de data e reduzindo em um dia
+    este valor */
+    setDate(subDays(date, 1));
+  }
+
+  function handleNextDay() {
+    /* setando um novo valor para o nosso estado de data e adicionando em um dia
+    este valor */
+    setDate(addDays(date, 1));
+  }
+
   return (
     <Container>
       <header>
-        <button type="button">
+        <button type="button" onClick={handlePrevDay}>
           <MdChevronLeft size={36} color="#fff" />
         </button>
-        <strong>07 de novembro</strong>
-        <button type="button">
+        <strong>{dateFormatted}</strong>
+        <button type="button" onClick={handleNextDay}>
           <MdChevronRight size={36} color="#fff" />
         </button>
       </header>
