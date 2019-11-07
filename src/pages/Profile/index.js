30bffcd48d3ab85e58f7d2e10946cdc800d5ab5e
Manoel de Oliveira Prado Neto <manoelprado.aecjr@gmail.com>
2019-11-07 11:33:50 -0300

    logout

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
 
+import { signOut } from '~/store/modules/auth/actions';
 import { updateProfileRequest } from '~/store/modules/user/actions';
 
 import AvatarInput from './AvatarInput';
@@ -15,6 +16,10 @@ export default function Profile() {
   function handleSubmit(data) {
     // console.tron.log(data);
     dispatch(updateProfileRequest(data));
+  }
+
+  function handleSignOut() {
+    dispatch(signOut());
   }
 
   return (
@@ -43,7 +48,9 @@ export default function Profile() {
         <button type="submit">Atualizar perfil</button>
       </Form>
 
-      <button type="button">Sair do GoBarber</button>
+      <button type="button" onClick={handleSignOut}>
+        Sair do GoBarber
+      </button>
     </Container>
   );
 }
